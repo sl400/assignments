17bb8628ff3b9bdb75ae85ca269c4d5bbeaac299
sl400 <suzanneboard@suzanne-boards-macbook-pro.local>
2015-08-23 20:17:19 +1000

    further development

---
+++ Final project - Portfolio site/Sue Board Design/js/index.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+	$(".work").click(function(){
+
+		$(".sub-nav").slideToggle(500);
+
+	});
+
+    $("button#nav-burger").click(function(){
+
+        $("#side-bar nav").slideToggle(500);
+
+    });
+
+
+});
+
